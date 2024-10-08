23391ba049b27d61496ec748abbc2c4d614f2305
MrBarker47 <jebarker7@gmail.com>
2024-10-08 15:01:19 -0400

    Update the files

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-// The provided course information.
-const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
+
+//Create a function that includes three pararmeters
+  function getLearnerData() {
   
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
+    const CourseInfo = {
+      id: 451,
+      name: "Introduction to JavaScript"
+    };
+    
+    // The provided assignment group.
+    const AssignmentGroup = {
+      id: 12345,
+      name: "Fundamentals of JavaScript",
+      course_id: 451,
+      group_weight: 25,
+      assignments: [
+        {
+          id: 1,
+          name: "Declare a Variable",
+          due_at: "2023-01-25",
+          points_possible: 50
+        },
+        {
+          id: 2,
+          name: "Write a Function",
+          due_at: "2023-02-27",
+          points_possible: 150
+        },
+        {
+          id: 3,
+          name: "Code the World",
+          due_at: "3156-11-15",
+          points_possible: 500
+        }
+      ]
+    };
+
+      // The provided learner submission data.
+    const LearnerSubmissions = [
     {
       learner_id: 125,
       assignment_id: 1,
@@ -76,8 +79,31 @@ const CourseInfo = {
     }
   ];
 
-  
-  //Create a function that includes three pararmeters
-  function getLearnerData(CourseInfo,AssignmentGroup,LearnerSubmissions) {
 
+
+
+
+
+    const result = [
+      {
+        id: 125,
+        avg: 0.985, // (47 + 150) / (50 + 150)
+        1: 0.94, // 47 / 50
+        2: 1.0 // 150 / 150
+      },
+      {
+        id: 132,
+        avg: 0.82, // (39 + 125) / (50 + 150)
+        1: 0.78, // 39 / 50
+        2: 0.833 // late: (140 - 15) / 150
+      }
+    ];
+  
+    return result;
   }
+
+
+getLearnerData();
+
+  
+    
