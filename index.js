3ed257c6645eb28160bb1a0d4d0cd252158905b6
MrBarker47 <jebarker7@gmail.com>
2024-10-31 19:47:55 -0400

    Update files

--- index.js
+++ index.js
@@ -1,100 +1,125 @@
-
+// The provided course information.
 const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
-  
+  id: 451,
+  name: "Introduction to JavaScript"
+};
 
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
- 
-  const LearnerSubmissions = [
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+    // The ID of the course the assignment group belongs to 
+  course_id: 451,
+  // The percentage weight of the entire assignment group
+  group_weight: 25,
+  assignments: [
     {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
+      id: 1,
+      name: "Declare a Variable",
+      // the due date for the assignment
+      due_at: "2023-01-25",
+      // the maximum points possible for the assignment
+      points_possible: 50
     },
     {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
     },
     {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
+    }
+  ]
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
+  }
+];
+
+function getLearnerData(course, ag, submissions) {
+  // here, we would process this data to achieve the desired result.
+  const result = [
+    {
+      id: 125,
+      avg: 0.985, 
+      1: 0.94, 
+      2: 1.0 
     },
     {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
-      }
+      id: 132,
+      avg: 0.82, 
+      1: 0.78, 
+      2: 0.833 
     }
   ];
 
- function getLearnerData(course, assignments, learner) {
-       let x = CourseInfo;
-       console.log([CourseInfo]);
-    
-       let j = AssignmentGroup;
-       console.log([AssignmentGroup]);
-    
-         let i = LearnerSubmissions;
-         console.log([LearnerSubmissions]);
+  return result;
+}
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 
-         let newScore = LearnerSubmissions.score = (47 + 50) / (150 / 50);
+let info = CourseInfo;
+let assignment = AssignmentGroup;
+let newLearner = LearnerSubmissions;
 
-         console.log(newScore);
+try {
+  console.log(result);
+  //CourseInfo
+  //AssignmentGroup
+  //LearnerSubmissions
+}
+catch(error) {
+  console.log(error);
+}
 
 
- }
 
 
-getLearnerData();
-  
-    
+
+
+console.log("You're all set");
